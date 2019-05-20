bd2d6a65a909072329d89483e54daacf275e06cd
dongguk kim <kdd0419@naver.com>
2019-05-20 21:21:34 +0900

    Second commit
    change arcaea_parser.py

--- arcaea_parser.py
+++ arcaea_parser.py
@@ -15,25 +15,37 @@ songlist = songlist['songs']
 
 song_info = []
 for song in songlist:
-    song_info.append({'id':song['id'],'name':song['title_localized']['en'], 'level':[diff['rating'] for diff in song['difficulties']]})
+    song_info.append({
+        'id': song['id'], 'name': song['title_localized']['en'],
+        'level': [diff['rating'] for diff in song['difficulties']]})
 
-song_rlt=[]
+song_rlt = []
 diff = ['PAST', 'PRESENT', 'FUTURE']
-clear_mode=['Track Lost', 'Track Complete (Normal Gauge)', 'Full Recall', 'Pure Memory', 'Track Complete (Easy Gauge)', 'Track Complete (Hard Gauge)']
+clear_mode = [
+    'Track Lost', 'Track Complete (Normal Gauge)',
+    'Full Recall', 'Pure Memory',
+    'Track Complete (Easy Gauge)', 'Track Complete (Hard Gauge)']
 for i in range(len(song_info)*3):
     score = arc.rank_me(song_info[i // 3]['id'], i % 3, 0, 0)['value']
-    tmp_dic = {"song_name":song_info[i // 3]['name'], "difficulty": diff[i % 3], "level":song_info[i // 3]['level'][i % 3]}
+    tmp_dic = {
+        "song_name": song_info[i // 3]['name'], "difficulty": diff[i % 3],
+        "level": song_info[i // 3]['level'][i % 3]}
     if len(score) <= 0:
         song_rlt.append(tmp_dic)
     else:
         tmp_dic['best_clear_type'] = clear_mode[score[0]['best_clear_type']]
-        row = ['score', 'shiny_perfect_count', 'perfect_count', 'near_count','miss_count','rank']
+        row = [
+            'score', 'shiny_perfect_count', 'perfect_count',
+            'near_count', 'miss_count', 'rank']
         for r in row:
             tmp_dic[r] = score[0][r]
         song_rlt.append(tmp_dic)
 
 with open('arcaea result.csv', 'w', newline="\n", encoding='utf-8') as csv_f:
-    fieldnames = ['song_name', 'difficulty', 'level', 'score', 'shiny_perfect_count', 'perfect_count', 'near_count', 'miss_count', 'best_clear_type', 'rank']
+    fieldnames = [
+        'song_name', 'difficulty', 'level', 'score',
+        'shiny_perfect_count', 'perfect_count', 'near_count',
+        'miss_count', 'best_clear_type', 'rank']
     writer = csv.DictWriter(csv_f, fieldnames=fieldnames)
     writer.writeheader()
     for song in song_rlt:
