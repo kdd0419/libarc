357a2c5835afd604389489a0b05d9c936a224871
김동국 <kdd0419@naver.com>
2019-09-24 15:32:11 +0900

    Update libarc.py (Arcaea's 2.3.2 update)

--- Source/libarc.py
+++ Source/libarc.py
@@ -8,7 +8,7 @@ headers = {
     'Accept': '*/*',
     'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
     'Accept-Encoding': 'br, gzip, deflate',
-    'AppVersion': '2.3.0',
+    'AppVersion': '2.3.2',
     'User-Agent': 'Arc-mobile/2.2.0.0 CFNetwork/976 Darwin/18.2.0'
 }
 
